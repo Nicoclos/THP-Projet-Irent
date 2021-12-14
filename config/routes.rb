1c9531bc0918a8dcc18f9456c7ff0a3b986b2325
Ju4n-P <th3dr34mm4k3r@gmail.com>
2021-12-14 03:48:16 +0100

    update ActiveStorage Activated for avatar user profile

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :items
-  resources :users
+  resources :users do
+    resources :profile_picture, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
