fa837ec892fa36176065705886b23dc43328d767
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-12-15 11:58:03 +0100

    error item.id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,28 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order= Order.new
+    order= Order.new
+    order.save
     @item = Item.all.find_by(id:params[:id])
     @amount=@item.price.to_i*100
     session[:price]= @amount
+    # session[:item_id]= @item.id
   end
 
-  def create 
+  def create
     user_id = current_user.id.to_s
-    @order = Order.new(quantity: 1, stripe_id: user_id)
+     item = Item.all.find_by(id:current_user.id)
+     item= item[0]
+    #  item = item.to_s
+  
+    user_usr = current_user.id
+    puts "$$$$$$$$$$$"
+    puts "#{item}"
+    puts "#{user_id}"
+    puts "#{user_usr}"
+    puts "$$$$$$$$$$$"
+   @order = Order.new(quantity: 1, stripe_id: user_id,user_id:user_usr,item_id:item)
+   
     if @order.save
       puts "Yes man"
     else
