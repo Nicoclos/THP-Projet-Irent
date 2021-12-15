afef12e9a7d4eb2bf9730e5f5d57bd311e82de31
jordan-creyelman <jordancreyelman1992@gmail.com>
2021-12-15 14:57:12 +0100

    stripe finish

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,23 +10,16 @@ class OrdersController < ApplicationController
     @item = Item.all.find_by(id:params[:id])
     @amount=@item.price.to_i*100
     session[:price]= @amount
-    # session[:item_id]= @item.id
+    session[:a] =@item[:id]
   end
 
   def create
+    item= session[:a]
     user_id = current_user.id.to_s
-     item = Item.all.find_by(id:current_user.id)
-     item= item[0id:params[:id]]
-    #  item = item.to_s
-  
     user_usr = current_user.id
-    puts "$$$$$$$$$$$"
-    puts "#{item}"
-    puts "#{user_id}"
-    puts "#{user_usr}"
-    puts "$$$$$$$$$$$"
-   @order = Order.new(quantity: 1, stripe_id: user_id,user_id:user_usr,item_id:item)
-   
+    stripe_id= user_usr+item
+    @dl = Item.all.find_by(id:item)
+    @order = Order.new(quantity: 1, stripe_id: stripe_id,user_id:user_usr,item_id:item)
     if @order.save
       puts "Yes man"
     else
@@ -34,6 +27,7 @@ class OrdersController < ApplicationController
     end  
     @amount = session[:price]*100
     puts "#{@amount}"
+    @dl.delete
     begin
       customer = Stripe::Customer.create({
       email: params[:stripeEmail],
