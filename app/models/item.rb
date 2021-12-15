76ecf7061472af71c4a65f708fbf63558441b60b
Ju4n-P <th3dr34mm4k3r@gmail.com>
2021-12-15 22:11:50 +0100

    update db between user and item 1 N

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,5 +12,7 @@ class Item < ApplicationRecord
     has_many :categories, through: :join_table_item_category
     accepts_nested_attributes_for :images
 
+    belongs_to :user
+
 
 end
---
+++ db/migrate/20211215210026_add_index_to_items.rb
@@ -0,0 +1,6 @@
+class AddIndexToItems < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :items, :user, foreign_key: true
+    
+  end
+end
