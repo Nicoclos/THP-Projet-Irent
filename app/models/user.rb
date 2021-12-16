d8e320e605a4093c166e454aeeff217112012a55
lionelcoding <louis.lionel@gmail.com>
2021-12-16 21:21:39 +0100

    UserMailMailler class to send email

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     
     after_create :welcome_send
     def welcome_send
-      UserMailer.welcome_email(self).deliver_now
+      UserMailMailer.welcome_email(self).deliver_now
     end
 
   end
