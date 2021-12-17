a0b1c6578ccc96d6813e3ae97da90e3ebcd097ab
Ju4n-P <th3dr34mm4k3r@gmail.com>
2021-12-17 04:06:10 +0100

    fixe problem with index for item

--- db/migrate/20211215210026_add_index_to_items.rb
+++ db/migrate/20211215210026_add_index_to_items.rb
@@ -1,8 +1,8 @@
 class AddIndexToItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :items, :user, foreign_key: true
+    add_reference :items, :user, foreign_key: true, index: true
     
-    add_reference :items, :category, foreign_key: true
+    add_reference :items, :category, foreign_key: true, index: true
                                                       
   end
 end
