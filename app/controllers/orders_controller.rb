fa3e5537cd13a78187e53d757a06d408a99fd5a3
lionelcoding <louis.lionel@gmail.com>
2021-12-18 15:52:40 +0100

    I've create payement mailer view and set up controller oder, test is ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,4 @@
 class OrdersController < ApplicationController
-
   def index
     @item =Item.all
   end
@@ -14,55 +13,63 @@ class OrdersController < ApplicationController
   end
 
   def create
-
-    item= session[:a]
-    user_id = current_user.id.to_s
-    user_usr = current_user.id
-    stripe_id= user_usr+item
-    @dl = Item.all.find_by(id:item)
-    @order = Order.new(quantity: 1, stripe_id: stripe_id,user_id:user_usr,item_id:item)
-    if @order.save
-      puts "Yes man"
-    else
-      puts "error!"
-    end
+    item_id= session[:a]
+    user_id = current_user.id
+    user_usr = current_user
+    stripe_id= item_id + user_id
+    # @amount = params[:price].to_i
+    @amount = Item.find(item_id).pric
+    # @price_converted = @amount * 100
+    puts "€€€€€€€€€€€€€€€€€€€"
+    puts @amount
+    puts "€€€€€€€€€€€€€€€€€€€"
     
-    @amount = params[:price].to_i
-    @price_converted = @amount * 100
-
+    # if @order.save
+    #   puts "Yes man"
+    # else
+    #   puts "error!"
+    # end
+    
+    # @object_item[:available]=false
+    # @object_item.update(:available=>@object_item[:available])
+    
     # @amount = session[:price]*100
-    
     # puts "#{@dl.inspect}"
     # puts "#{@amount}"
-    # @dl[:available]=false
-    # @dl.update(:available=>@dl[:available])
-    begin
+    
       customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-      })
+        email: params[:stripeEmail],
+        source: params[:stripeToken],
+        })
       charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @price_converted,
-      description: "Achat d'un produit",
-      currency: 'eur',
-      })
-
-      if charge
-        @order = Order.find(params[:order_id])
-        # @order.is_paid = true
-        # @order.save
-        flash[:success] = "Bravo, tu as réservé ta séance et payé #{@amount} €!"
-        payment_buyer_confirmation(@order)
-        # payment_vendor_confirmation(@order)
-        redirect_to user_path(current_user.id)
+        customer: customer.id,
+        amount: @amount,
+        description: "Achat d'un produit",
+        currency: 'eur',
+        })
+          
+        if charge
+            # @object_item = Item.all.find_by(id:item_id)
+            # @order = Order.find(1)
+            # @order.is_paid = true
+            # @order.save
+            flash[:success] = "Bravo, !"
+            Order.create(quantity: 1, stripe_id: stripe_id,user_id: user_id,item_id: item_id)
+            payment_buyer_confirmation(@order)
+            # payment_vendor_confirmation(@order)
+        else
+          puts = "ça ne fonctionne pas"
+        end
+          rescue Stripe::CardError => e
+            flash[:error] = e.message
+            redirect_to new_order_path
+        end
+        
+    
+      
+      
+      def payment_buyer_confirmation(order)
+        UserMailer.payment_email(order).deliver_now
       end
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_order_path
-    end
-    def payment_buyer_confirmation(order)
-      UserMailer.payment_buyer_confirmation(order).deliver_now
-    end
-  end
+      
 end
